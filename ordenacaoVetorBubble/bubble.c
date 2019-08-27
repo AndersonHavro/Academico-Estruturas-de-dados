822921733efb34c3fe618e99927fb92054f0e91c
Anderson Havro <andersonhavro@gmail.com>
2019-08-27 16:30:23 -0300

    ordenacao bubble

---
+++ ordenacaoVetorBubble/bubble.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	int vect[] = { 20,15,2,5,7,1,3 };
+	int tam = sizeof(vect) / sizeof(int);
+
+	int aux = 0;
+	for (int x = tam - 1; x > 0; x--)
+	{
+		for (int y = 0; y < x; y++)
+		{
+			if (vect[y] > vect[y + 1])
+			{
+				aux = vect[y];
+				vect[y] = vect[y + 1];
+				vect[y + 1] = aux;
+
+			}
+		}
+	}
+	for (int i = 0; i < tam; i++)
+	{
+		printf("%i, ", vect[i]);
+	}
+	getchar();
+	return 0;
+}
